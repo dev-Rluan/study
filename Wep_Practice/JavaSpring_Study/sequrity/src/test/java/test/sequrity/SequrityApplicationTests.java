598f984041a689586222c08c85a805288bbffda5
dev_whale <jeong22437747@gmail.com>
2022-08-25 00:35:58 +0900

    security test3

---
+++ Wep_Practice/JavaSpring_Study/sequrity/src/test/java/test/sequrity/SequrityApplicationTests.java
@@ -0,0 +1,13 @@
+package test.sequrity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SequrityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
